84dd59002aee0d53edb263fc3016b61cd1f0a13f
Seungjin Park <aad336545@gmail.com>
2024-01-30 18:44:13 +0900

    fix network

--- src/ethereum/networks/index.js
+++ src/ethereum/networks/index.js
@@ -28,25 +28,25 @@ const networks = [
         rpcUrl: 'https://rpc-mumbai.matic.today',
         explorer: 'https://mumbai.polygonscan.com',
         contractAddress: {
-            GameFactory: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+            GameFactory: '0xeecae2a9eeb2de031dd7e66ead3618f7083ad830',
         },
     },
-    {
-        chainId: '0x66eed',
-        chainName: 'Arbitrum Goerli',
-        renderName: 'Arbitrum goerli',
-        currencyInfo: {
-            symbol: 'AGOR',
-            decimals: 18,
-            name: 'agor',
-        },
-        websocketUrl: 'wss://arbitrum-goerli.publicnode.com',
-        rpcUrl: 'https://arbitrum-goerli.publicnode.com',
-        explorer: '',
-        contractAddress: {
-            GameFactory: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
-        },
-    },
+    // {
+    //     chainId: '0x66eed',
+    //     chainName: 'Arbitrum Goerli',
+    //     renderName: 'Arbitrum goerli',
+    //     currencyInfo: {
+    //         symbol: 'AGOR',
+    //         decimals: 18,
+    //         name: 'agor',
+    //     },
+    //     websocketUrl: 'wss://arbitrum-goerli.publicnode.com',
+    //     rpcUrl: 'https://arbitrum-goerli.publicnode.com',
+    //     explorer: '',
+    //     contractAddress: {
+    //         GameFactory: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+    //     },
+    // },
 ];
 
 export function getSupportedNetworks() {
